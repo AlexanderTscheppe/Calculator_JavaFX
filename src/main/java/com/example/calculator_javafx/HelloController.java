e4c1f4e40a83492554802a90a1671f3523a9c90c
AlexanderTscheppe <alexander.tscheppe@a1.net>
2022-07-22 23:12:51 +0200

    Added functionality to numerical buttons

--- src/main/java/com/example/calculator_javafx/HelloController.java
+++ src/main/java/com/example/calculator_javafx/HelloController.java
@@ -1,21 +1,102 @@
 package com.example.calculator_javafx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.event.ActionEvent;
-import javafx.application.Platform;
-import javafx.stage.Stage;
+
 
 public class HelloController {
     @FXML
-    private Button buttonEnter = new Button();
+    private TextArea textAreaInput = new TextArea("");
+    @FXML
+    private Button button0 = new Button();
+    @FXML
+    private Button button1 = new Button();
+    @FXML
+    private Button button2 = new Button();
+    @FXML
+    private Button button3 = new Button();
+    @FXML
+    private Button button4 = new Button();
+    @FXML
+    private Button button5 = new Button();
+    @FXML
+    private Button button6 = new Button();
+    @FXML
+    private Button button7 = new Button();
+    @FXML
+    private Button button8 = new Button();
+    @FXML
+    private Button button9 = new Button();
+
 
     @FXML
-    public void onHelloButtonClick() {
-        buttonEnter.setOnAction((ActionEvent event) -> {
-        Stage stage = (Stage) buttonEnter.getScene().getWindow();
-        stage.close();
-       });
+    private void button0Pressed(){
+        if(textAreaInput.getText() != null) {
+            textAreaInput.setText(textAreaInput.getText() + "0");
+        }
     }
-}
+    @FXML
+    private void button1Pressed(){
+        if(textAreaInput.getText() != null) {
+            textAreaInput.setText(textAreaInput.getText() + "1");
+        }
+    }
+    @FXML
+    private void button2Pressed(){
+        if(textAreaInput.getText() != null) {
+            textAreaInput.setText(textAreaInput.getText() + "2");
+        }
+    }
+    @FXML
+    private void button3Pressed(){
+        if(textAreaInput.getText() != null) {
+            textAreaInput.setText(textAreaInput.getText() + "3");
+        }
+    }
+    @FXML
+    private void button4Pressed(){
+        if(textAreaInput.getText() != null) {
+            textAreaInput.setText(textAreaInput.getText() + "4");
+        }
+    }
+    @FXML
+    private void button5Pressed(){
+        if(textAreaInput.getText() != null) {
+            textAreaInput.setText(textAreaInput.getText() + "5");
+        }
+    }
+    @FXML
+    private void button6Pressed(){
+        if(textAreaInput.getText() != null) {
+            textAreaInput.setText(textAreaInput.getText() + "6");
+        }
+    }
+    @FXML
+    private void button7Pressed(){
+        if(textAreaInput.getText() != null) {
+            textAreaInput.setText(textAreaInput.getText() + "7");
+        }
+    }
+    @FXML
+    private void button8Pressed(){
+        if(textAreaInput.getText() != null) {
+            textAreaInput.setText(textAreaInput.getText() + "8");
+        }
+    }
+    @FXML
+    private void button9Pressed(){
+        if(textAreaInput.getText() != null) {
+            textAreaInput.setText(textAreaInput.getText() + "9");
+        }
+    }
+
+
+
+
+
+
+
+
+    }
+
