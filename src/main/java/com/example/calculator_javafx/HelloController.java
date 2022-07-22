ccbf6c99b6699dc84d7ddafbda368c1a8a11b389
AlexanderTscheppe <alexander.tscheppe@a1.net>
2022-07-22 19:11:32 +0200

    Calculator GUI, no functionality yet. Deleted one fxml file

--- src/main/java/com/example/calculator_javafx/HelloController.java
+++ src/main/java/com/example/calculator_javafx/HelloController.java
@@ -2,13 +2,20 @@ package com.example.calculator_javafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.application.Platform;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button buttonEnter = new Button();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onHelloButtonClick() {
+        buttonEnter.setOnAction((ActionEvent event) -> {
+        Stage stage = (Stage) buttonEnter.getScene().getWindow();
+        stage.close();
+       });
     }
 }
