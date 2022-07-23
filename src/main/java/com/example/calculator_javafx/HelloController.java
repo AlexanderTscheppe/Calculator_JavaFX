a9062b2c4bfa713b9bc807626e5ed4f11a9dbb87
AlexanderTscheppe <alexander.tscheppe@a1.net>
2022-07-23 17:16:15 +0200

    Added functionality, added comments. Works just fine

--- src/main/java/com/example/calculator_javafx/HelloController.java
+++ src/main/java/com/example/calculator_javafx/HelloController.java
@@ -2,45 +2,27 @@ package com.example.calculator_javafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
 
 
 public class HelloController {
     @FXML
-    private TextArea textAreaInput1 = new TextArea("");
+    private TextArea textAreaInput1 = new TextArea(""); //Input-Area in the top left corner.
     @FXML
-    private TextField textFieldOperation = new TextField("");
+    private TextField textFieldOperation = new TextField(""); //Input-Field for the operation (+,-,*,/)
     @FXML
-    private TextArea textAreaInput2 = new TextArea("");
+    private TextArea textAreaInput2 = new TextArea(""); //Input-Area in the top right corner
     @FXML
-    private TextArea textAreaOutput = new TextArea("");
-    /* @FXML
-    private Button button0 = new Button();
-    @FXML
-    private Button button1 = new Button();
-    @FXML
-    private Button button2 = new Button();
-    @FXML
-    private Button button3 = new Button();
-    @FXML
-    private Button button4 = new Button();
-    @FXML
-    private Button button5 = new Button();
-    @FXML
-    private Button button6 = new Button();
-    @FXML
-    private Button button7 = new Button();
-    @FXML
-    private Button button8 = new Button();
-    @FXML
-    private Button button9 = new Button(); */
-    private double input1 = 0d;
-    private double input2 = 0d;
-    private double output = 0d;
-    private boolean operation = false;
-    private boolean firstNumber = false;
+    private TextArea textAreaOutput = new TextArea(""); //Output-Area for the result, which can be copied into textAreaInput1
+    private double input1 = 0d; //Inputvalue from textAreaInput1
+    private double input2 = 0d; //Inputvalue from textAreaInput2
+    private double output = 0d; //Outputvalue for textAreaOutput
+    private boolean operation = false; //Boolean for checking whether an operation was applied or not
+    private boolean firstNumber = false; //Boolean for checking whether an input exists in textAreaInput1
+    private boolean bothNumbers = false; //Boolean for checking whether an input exists in textAreaInput1 and textAreaInput2
 
 
+
+    //button0Pressed-button9Pressed: functions to recognize the pressing of the numerical buttons.
     @FXML
     private void button0Pressed() {
         if (operation == false) {
@@ -140,8 +122,18 @@ public class HelloController {
         }
     }
 
+
+
     @FXML
-    private void buttonPlusPressed() {
+    private void buttonCommaPressed(){ //function to apply a decimal point to the number in die input-area
+        if(operation == false){
+            textAreaInput1.setText(textAreaInput1.getText() + ".");
+        } else{
+            textAreaInput2.setText(textAreaInput2.getText() + ".");
+        }
+    }
+    @FXML
+    private void buttonPlusPressed() { //function to apply a plus in the operation-field
         if (textFieldOperation.getText().equals("")) {
             textFieldOperation.setText("+");
             operation = true;
@@ -149,7 +141,7 @@ public class HelloController {
     }
 
     @FXML
-    private void buttonMinusPressed() {
+    private void buttonMinusPressed() { //function to apply a minus in the operation-field
         if (textFieldOperation.getText().equals("")) {
             textFieldOperation.setText("-");
             operation = true;
@@ -157,7 +149,7 @@ public class HelloController {
     }
 
     @FXML
-    private void buttonTimesPressed() {
+    private void buttonTimesPressed() { //function to apply a muliplication operator to the operation-field
         if (textFieldOperation.getText().equals("")) {
             textFieldOperation.setText("*");
             operation = true;
@@ -165,7 +157,7 @@ public class HelloController {
     }
 
     @FXML
-    private void buttonDividePressed() {
+    private void buttonDividePressed() { //function to apply a division operatior to the operation-field
         if (textFieldOperation.getText().equals("")) {
             textFieldOperation.setText("/");
             operation = true;
@@ -173,32 +165,57 @@ public class HelloController {
     }
 
     @FXML
-    private void buttonEnterPressed() {
+    private void buttonClearPressed(){ //function to clear all textareas and textfields. Resets all values
+        input1 = 0d;
+        input2 = 0d;
+        output = 0d;
+        operation = false;
+        firstNumber = false;
+        bothNumbers = false;
+        textAreaInput1.setText("");
+        textAreaInput2.setText("");
+        textFieldOperation.setText("");
+        textAreaOutput.setText("");
+    }
+    @FXML
+    private void buttonCopyPressed(){ //function to copy the result to textAreaInput1 for consecutive operations
+        input1 = output;
+        input2 = 0d;
+        output = 0d;
+        operation = false;
+        firstNumber = true;
+        bothNumbers = false;
+        textAreaInput1.setText(Double.toString(input1));
+        textAreaInput2.setText("");
+        textFieldOperation.setText("");
+        textAreaOutput.setText("");
+    }
+    @FXML
+    private void buttonEnterPressed() { //function to apply the operator to the inputs
         if (firstNumber == true) {
+            EnterPressed();
             Result();
             System.out.println(output);
         }
     }
 
     @FXML
-    private void buttonOperationPressed() {
+    private void buttonOperationPressed(){ //function to switch from textAreaInput1 to textAreaInput2. Sets value of input1 to the given number
         if (firstNumber == false) {
             input1 = Double.parseDouble(textAreaInput1.getText());
             firstNumber = true;
-            System.out.println(input1);
-        } else {
-            EnterPressed();
+        }
 
         }
-    }
 
-    private void EnterPressed() {
+
+    private void EnterPressed() { //function to set the value of input2 to the given number
         input2 = Double.parseDouble(textAreaInput2.getText());
-        System.out.println(input2);
+        bothNumbers = true;
 
     }
 
-    private double Result() {
+    private double Result() { //function for applying the individual operations. Divison by 0 is prohibited
         if (textFieldOperation.getText().equals("+")) {
             output = input1 + input2;
             textAreaOutput.setText(Double.toString(output));
@@ -212,16 +229,18 @@ public class HelloController {
             textAreaOutput.setText(Double.toString(output));
             return output;
         } else if(textFieldOperation.getText().equals("/")) {
-            if(input2 == 0){
+            if(input2 == 0) {
                 textAreaOutput.setText("Division by 0 is not possible");
                 return output;
-        } else {
+            }else {
                 output = input1/input2;
                 textAreaOutput.setText(Double.toString(output));
                 return output;
             }
+        }
+    return output;
     }
-        return output;
+
     }
-}
+
 
