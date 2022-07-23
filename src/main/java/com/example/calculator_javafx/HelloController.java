8650199c37723f02bd525d939a0ce4ca2571f0d7
AlexanderTscheppe <alexander.tscheppe@a1.net>
2022-07-23 15:39:02 +0200

    Added function to the operation buttons +,-,*,/

--- src/main/java/com/example/calculator_javafx/HelloController.java
+++ src/main/java/com/example/calculator_javafx/HelloController.java
@@ -7,8 +7,14 @@ import javafx.scene.control.Button;
 
 public class HelloController {
     @FXML
-    private TextArea textAreaInput = new TextArea("");
+    private TextArea textAreaInput1 = new TextArea("");
     @FXML
+    private TextField textFieldOperation = new TextField("");
+    @FXML
+    private TextArea textAreaInput2 = new TextArea("");
+    @FXML
+    private TextArea textAreaOutput = new TextArea("");
+    /* @FXML
     private Button button0 = new Button();
     @FXML
     private Button button1 = new Button();
@@ -27,76 +33,195 @@ public class HelloController {
     @FXML
     private Button button8 = new Button();
     @FXML
-    private Button button9 = new Button();
+    private Button button9 = new Button(); */
+    private double input1 = 0d;
+    private double input2 = 0d;
+    private double output = 0d;
+    private boolean operation = false;
+    private boolean firstNumber = false;
 
 
     @FXML
-    private void button0Pressed(){
-        if(textAreaInput.getText() != null) {
-            textAreaInput.setText(textAreaInput.getText() + "0");
-        }
-    }
-    @FXML
-    private void button1Pressed(){
-        if(textAreaInput.getText() != null) {
-            textAreaInput.setText(textAreaInput.getText() + "1");
-        }
-    }
-    @FXML
-    private void button2Pressed(){
-        if(textAreaInput.getText() != null) {
-            textAreaInput.setText(textAreaInput.getText() + "2");
-        }
-    }
-    @FXML
-    private void button3Pressed(){
-        if(textAreaInput.getText() != null) {
-            textAreaInput.setText(textAreaInput.getText() + "3");
-        }
-    }
-    @FXML
-    private void button4Pressed(){
-        if(textAreaInput.getText() != null) {
-            textAreaInput.setText(textAreaInput.getText() + "4");
-        }
-    }
-    @FXML
-    private void button5Pressed(){
-        if(textAreaInput.getText() != null) {
-            textAreaInput.setText(textAreaInput.getText() + "5");
-        }
-    }
-    @FXML
-    private void button6Pressed(){
-        if(textAreaInput.getText() != null) {
-            textAreaInput.setText(textAreaInput.getText() + "6");
-        }
-    }
-    @FXML
-    private void button7Pressed(){
-        if(textAreaInput.getText() != null) {
-            textAreaInput.setText(textAreaInput.getText() + "7");
-        }
-    }
-    @FXML
-    private void button8Pressed(){
-        if(textAreaInput.getText() != null) {
-            textAreaInput.setText(textAreaInput.getText() + "8");
-        }
-    }
-    @FXML
-    private void button9Pressed(){
-        if(textAreaInput.getText() != null) {
-            textAreaInput.setText(textAreaInput.getText() + "9");
+    private void button0Pressed() {
+        if (operation == false) {
+            textAreaInput1.setText(textAreaInput1.getText() + "0");
+        } else {
+            textAreaInput2.setText(textAreaInput2.getText() + "0");
         }
     }
 
+    @FXML
+    private void button1Pressed() {
+        if (operation == false) {
+            textAreaInput1.setText(textAreaInput1.getText() + "1");
 
+        } else {
+            textAreaInput2.setText(textAreaInput2.getText() + "1");
+        }
+    }
 
+    @FXML
+    private void button2Pressed() {
+        if (operation == false) {
+            textAreaInput1.setText(textAreaInput1.getText() + "2");
 
+        } else {
+            textAreaInput2.setText(textAreaInput2.getText() + "2");
+        }
+    }
 
+    @FXML
+    private void button3Pressed() {
+        if (operation == false) {
+            textAreaInput1.setText(textAreaInput1.getText() + "3");
 
+        } else {
+            textAreaInput2.setText(textAreaInput2.getText() + "3");
+        }
+    }
 
+    @FXML
+    private void button4Pressed() {
+        if (operation == false) {
+            textAreaInput1.setText(textAreaInput1.getText() + "4");
+
+        } else {
+            textAreaInput2.setText(textAreaInput2.getText() + "4");
+        }
+    }
+
+    @FXML
+    private void button5Pressed() {
+        if (operation == false) {
+            textAreaInput1.setText(textAreaInput1.getText() + "5");
+
+        } else {
+            textAreaInput2.setText(textAreaInput2.getText() + "5");
+        }
+    }
+
+    @FXML
+    private void button6Pressed() {
+        if (operation == false) {
+            textAreaInput1.setText(textAreaInput1.getText() + "6");
+
+        } else {
+            textAreaInput2.setText(textAreaInput2.getText() + "6");
+        }
+    }
+
+    @FXML
+    private void button7Pressed() {
+        if (operation == false) {
+            textAreaInput1.setText(textAreaInput1.getText() + "7");
+
+        } else {
+            textAreaInput2.setText(textAreaInput2.getText() + "7");
+        }
+    }
+
+    @FXML
+    private void button8Pressed() {
+        if (operation == false) {
+            textAreaInput1.setText(textAreaInput1.getText() + "8");
+
+        } else {
+            textAreaInput2.setText(textAreaInput2.getText() + "8");
+        }
+    }
+
+    @FXML
+    private void button9Pressed() {
+        if (operation == false) {
+            textAreaInput1.setText(textAreaInput1.getText() + "9");
+
+        } else {
+            textAreaInput2.setText(textAreaInput2.getText() + "9");
+        }
+    }
+
+    @FXML
+    private void buttonPlusPressed() {
+        if (textFieldOperation.getText().equals("")) {
+            textFieldOperation.setText("+");
+            operation = true;
+        }
+    }
+
+    @FXML
+    private void buttonMinusPressed() {
+        if (textFieldOperation.getText().equals("")) {
+            textFieldOperation.setText("-");
+            operation = true;
+        }
+    }
+
+    @FXML
+    private void buttonTimesPressed() {
+        if (textFieldOperation.getText().equals("")) {
+            textFieldOperation.setText("*");
+            operation = true;
+        }
+    }
+
+    @FXML
+    private void buttonDividePressed() {
+        if (textFieldOperation.getText().equals("")) {
+            textFieldOperation.setText("/");
+            operation = true;
+        }
+    }
+
+    @FXML
+    private void buttonEnterPressed() {
+        if (firstNumber == true) {
+            Result();
+            System.out.println(output);
+        }
+    }
+
+    @FXML
+    private void buttonOperationPressed() {
+        if (firstNumber == false) {
+            input1 = Double.parseDouble(textAreaInput1.getText());
+            firstNumber = true;
+            System.out.println(input1);
+        } else {
+            EnterPressed();
+
+        }
+    }
+
+    private void EnterPressed() {
+        input2 = Double.parseDouble(textAreaInput2.getText());
+        System.out.println(input2);
 
     }
+
+    private double Result() {
+        if (textFieldOperation.getText().equals("+")) {
+            output = input1 + input2;
+            textAreaOutput.setText(Double.toString(output));
+            return output;
+        } else if(textFieldOperation.getText().equals("-")) {
+            output = input1 - input2;
+            textAreaOutput.setText(Double.toString(output));
+            return output;
+        } else if(textFieldOperation.getText().equals("*")) {
+            output = input1*input2;
+            textAreaOutput.setText(Double.toString(output));
+            return output;
+        } else if(textFieldOperation.getText().equals("/")) {
+            if(input2 == 0){
+                textAreaOutput.setText("Division by 0 is not possible");
+                return output;
+        } else {
+                output = input1/input2;
+                textAreaOutput.setText(Double.toString(output));
+                return output;
+            }
+    }
+        return output;
+    }
+}
 
